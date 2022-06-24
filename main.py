2ef69613e4ad6ab73fcbf227327d2688d8db2cee
George <george.neokq@gmail.com>
2022-06-24 15:53:34 +0800

    refactor: Change OSPF to EIGRP in main.py and simplify CLI syntax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-from ospf_hijack import *
+from eigrp_hijack import *
 from dns_hijack import *
 
 def main():
@@ -7,28 +7,27 @@ def main():
   subparsers = parser.add_subparsers(dest='attack')
   subparsers.required = True
   
-  ospf_subparser = subparsers.add_parser('ospf')
-  ospf_subparser.add_argument('--networks',
+  ospf_subparser = subparsers.add_parser('eigrp')
+  ospf_subparser.add_argument('networks',
     nargs='+',
     type=str,
     action='store',
-    help="Space-separated network addresses in CIDR notation",
-    required=True)
+    help="Space-separated network addresses in CIDR notation"
+  )
 
   dns_subparser = subparsers.add_parser('dns')
-  dns_subparser.add_argument('--map',
+  dns_subparser.add_argument('map',
     nargs='+',
     type=str,
     action='store',
     help="Space separated mappings, each key-value being colon-separated",
-    required=True
   )
 
   args = parser.parse_args()
   attack = args.attack
 
-  if attack == 'ospf':
-    spam_hello_and_advertisements(networks=args.networks)
+  if attack == 'eigrp':
+    eigrp_inject_routes(networks=args.networks)
   
   if attack == 'dns':
     # Colon separated key-value
